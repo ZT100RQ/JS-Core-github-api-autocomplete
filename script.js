b9b53d0285507a19ba7bec3e79ccb094256cd6c9
Azat Khabirov <zt100rq@gmail.com>
2024-08-04 19:14:06 +0500

    fix

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const searchInput = document.querySelector(".search-text");
 const searchAuto = document.querySelector(".search-autocom");
 const searchResult = document.querySelector(".search-result");
 
+//  DEBOUNCE
+
 const debounce = (fn, ms) => {
   let timer;
   function newFn() {
@@ -15,32 +17,69 @@ const debounce = (fn, ms) => {
   return newFn;
 };
 
-function getRepo(e) {
-  return fetch("https://api.github.com/search/repositories?q=" + e).then(
-    (response) => response.json()
-  );
+// Запрос на сервер.
+
+async function getRepositories(input) {
+  try {
+    const responseData = await fetch(
+      `https://api.github.com/search/repositories?q=${input}`
+    );
+    const responseRepositories = await responseData.json();
+    return responseRepositories.items;
+  } catch (err) {
+    console.log(`Ошибка запроса: ${err}`);
+  }
 }
 
-function evenT(e) {
-  getRepo(e.target.value).then((repos) => {
-    let userData = e.target.value;
-    let empty = [];
-    if (userData) {
-      empty = repos.items.slice(0, 5);
-      empty = empty.map((data) => {
-        return (data = "<li>" + data.name + "</li>");
-      });
-      searchForm.classList.add("active");
-    } else {
-      searchForm.classList.remove("active");
-    }
-    showSuggestions(empty);
-  });
+function repositoryEvent(e) {
+  if (searchInput.value == 0 || searchInput.value == " ") {
+    return;
+  }
+  getRepositories(e.target.value)
+    .then((repos) => {
+      let userData = e.target.value;
+      let empty = [];
+      if (userData) {
+        empty = repos.slice(0, 5);
+        empty = empty.map((data) => {
+          return (data = `<li id=${empty.findIndex(
+            (el) => el.name === data.name
+          )}>${data.name}</li>`);
+        });
+        searchForm.classList.add("active");
+      } else {
+        searchForm.classList.remove("active");
+      }
+      showSuggestions(empty);
+      newArr = repos.slice(0, 5);
+      searchAuto.addEventListener("click", createElem);
+    })
+    .catch((err) => {
+      alert(`Что-то пошло не так: ${err}`);
+    });
 }
 
-let newEvent = debounce(evenT, 300);
-
-searchInput.addEventListener("keyup", newEvent);
+function createElem(e) {
+  myTarget = newArr[e.target.id];
+  const li = document.createElement("li");
+  const p1 = document.createElement("p");
+  p1.textContent = `Name: ${myTarget.name}`;
+  const p2 = document.createElement("p");
+  p2.textContent = `Owner: ${myTarget.owner.login}`;
+  const p3 = document.createElement("p");
+  p3.textContent = `Stars: ${myTarget.stargazers_count}`;
+  const btn = document.createElement("button");
+  btn.classList.add(`delete`);
+  li.classList.add(`li-${myTarget.id}`);
+  li.appendChild(p1);
+  li.appendChild(p2);
+  li.appendChild(p3);
+  li.appendChild(btn);
+  searchResult.insertAdjacentElement("afterbegin", li);
+  e.target.removeEventListener("click", createElem);
+  searchInput.value = "";
+  searchAuto.innerHTML = "";
+}
 
 function showSuggestions(list) {
   let listData;
@@ -53,20 +92,12 @@ function showSuggestions(list) {
   searchAuto.innerHTML = listData;
 }
 
-// function newLi(oneRepo) {
-//   const li = document.createElement("li");
-//   const p1 = document.createElement("p");
-//   p1.textContent = oneRepo.name;
-//   const p2 = document.createElement("p");
-//   p2.textContent = oneRepo.owner.login;
-//   const p3 = document.createElement("p");
-//   p3.textContent = oneRepo.stargazers_count;
-//   const btn = document.createElement("button");
-//   btn.classList.add(`id-${oneRepo.id}`);
-//   li.classList.add(`li-${oneRepo.id}`);
-//   li.insertAdjacentHTML("afterbegin", btn);
-//   li.insertAdjacentHTML("afterbegin", p3);
-//   li.insertAdjacentHTML("afterbegin", p2);
-//   li.insertAdjacentHTML("afterbegin", p1);
-//   return li;
-// }
+let newEvent = debounce(repositoryEvent, 500);
+
+searchInput.addEventListener("input", newEvent);
+
+searchResult.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.closest("li").remove();
+  }
+});
